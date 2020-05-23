d84c7d4412f34fb231f63c57caa1caef2213e725
Cansu Cantürk <cansucanturk@users.noreply.github.com>
2020-05-23 14:49:07 +0300

    process of listing users

--- app/src/main/java/com/example/chatapp/GirisActivity.java
+++ app/src/main/java/com/example/chatapp/GirisActivity.java
@@ -3,6 +3,7 @@ package com.example.chatapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -46,14 +47,17 @@ public class GirisActivity extends AppCompatActivity {
 
     }
 
-    public void ekle(String kullaniciAdi) {
+    public void ekle(final String kullaniciAdi) {
         reference.child("Kullanıcılar").child(kullaniciAdi).child("kullaniciadi").setValue(kullaniciAdi).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
                 if (task.isSuccessful()) {
                     Toast.makeText(getApplicationContext(), "Kayıt başarılı", Toast.LENGTH_LONG).show();
+                  Intent intent = new Intent(GirisActivity.this,MainActivity.class);
+                 Intent.putExtra("kullaniciAdi", kullaniciAdi);
+                  startActivity(intent);
                 }
-
             }
+
         });
     }}
--- app/src/main/java/com/example/chatapp/MainActivity.java
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -1,16 +1,49 @@
 package com.example.chatapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class MainActivity extends AppCompatActivity {
 
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference reference;
 
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tanimla();
+    }
+
+    public void tanimla()
+    {
+        String userName = getIntent().getExtras().getString(kullaniciAdi);
+        firebaseDatabase = firebaseDatabase.getInstance();
+        reference =firebaseDatabase.getReference();
+    }
+
+    public void listele{
+
+        reference.child("Kullanicilar").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        })
+        )
     }
 }
